3b61c734cf0ed026016cfc4d52ca51686a46d2f0
Ricardo Picinatto <ederli.picinatto@gmail.com>
2020-04-29 18:46:56 -0300

    arange folders and np_operations

---
+++ numpy/np_operations.py
@@ -0,0 +1,17 @@
+import numpy as np
+# Set the base array
+arr = np.arange(0,11)
+print(arr)
+
+# Can perfome simple arithmatic operaitons in arrays
+print(arr + arr)
+# Scalar operations
+print(arr * 10)
+
+# Universal array functions
+
+# Calculating the square root
+print(np.sqrt(arr))
+
+# Calculating the max element
+print(np.max(arr))
