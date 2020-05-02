2054d6a3e36a1f83e45777bae941742e322bc0e1
Ricardo Picinatto <ederli.picinatto@gmail.com>
2020-05-02 11:08:25 -0300

    Exercise salaries

--- pd_exercise_salaries/pd_exercise_salaries.py
+++ pd_exercise_salaries/pd_exercise_salaries.py
@@ -22,3 +22,18 @@ print(sal.loc[sal['EmployeeName']=='JOSEPH DRISCOLL']['JobTitle'])
 
 # How much JOSEPH DRISCOLL makes including benefits
 print(sal[sal['EmployeeName']=='JOSEPH DRISCOLL']['TotalPayBenefits'])
+
+# What is the name of highest paid person including benefits
+print(sal.loc[sal['TotalPayBenefits']==sal['TotalPayBenefits'].max()]['EmployeeName'])
+
+# What is the name of the lowes payed person including benefits
+print(sal[sal['TotalPayBenefits']==sal['TotalPayBenefits'].min()]['EmployeeName'])
+
+# What is the mean Base pay for all employees per year
+print(sal.groupby('Year')['BasePay'].mean())
+
+# How many unique job title are there
+print(sal['JobTitle'].nunique())
+
+# What are the top 5 most common jobs
+print(sal['JobTitle'].value_counts().head())
