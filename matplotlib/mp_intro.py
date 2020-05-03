52602dd8db1515302eddd36daddb59677bcba582
Ricardo Picinatto <ederli.picinatto@gmail.com>
2020-05-03 10:01:53 -0300

    Add OO figures

--- matplotlib/mp_intro.py
+++ matplotlib/mp_intro.py
@@ -5,7 +5,9 @@ import matplotlib.pyplot as plt
 x = np.linspace(0,5,11)
 y = x ** 2
 
-## Functions method
+## 
+## FUNCTIONS METHOS
+##
 
 # Create the figure with functions
 plt.plot(x,y)
@@ -14,16 +16,19 @@ plt.ylabel('Y Label')
 plt.title('Title')
 #plt.show()
 
+# MULTIPLOTS
 # Multiplots in the same canvas
 plt.subplot(1,2,1)
 plt.plot(x,y,'r')
-
 plt.subplot(1,2,2)
 plt.plot(y,x,'b')
 #plt.show()
 
+##
+## OBJECT ORIENTED METHOD
+##
 
-## Using the Object Oriented Method
+## CREATING FIGURES WITH OO METHOD
 fig = plt.figure()
 axes = fig.add_axes([0.1,0.1,0.8,0.8])
 axes.plot(x,y)
@@ -32,11 +37,24 @@ axes.set_ylabel('Y Label')
 axes.set_title('Set title')
 #plt.show()
 
-
+# SUBPLOTS MANUALLY
 fig = plt.figure()
 axes1 = fig.add_axes([0.1,0.1,0.5,0.8])
 axes2 = fig.add_axes([0.2,0.5,0.4,0.3])
-
 axes1.plot(x,y)
 axes2.plot(y,x)
+#plt.show()
+
+
+# SUBPLOTS SHORT SINTAX
+# Short sintax for starting the subplots
+# The axes in this case is created automatically a list of axes
+fig,axes = plt.subplots(nrows=1,ncols=2)
+# Can plot both with the same data: axes.plot(x,y)
+# Or plot with diferente data:
+axes[0].plot(x,y)
+axes[0].set_title('First Plot')
+axes[1].plot(y,x)
+axes[1].set_title('Second Plot')
 plt.show()
+
